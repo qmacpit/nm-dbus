131e294a878708a8189168299fd90c411a7f7814
maciej.pitucha <m.pitucha@camlingroup.com>
2014-10-28 16:22:20 +0100

    trying to activate connection

--- tests/activeConnection_tests.js
+++ tests/activeConnection_tests.js
@@ -2,56 +2,55 @@ var assert = require("assert"),
     expect = require('expect.js'),
     NmManager = require("../lib/nmManager");
 
-var _defaultConnection = {
-    "connection": {
-        "id": "dummyConnection",
-        "uuid": "8920a029-957d-4da4-9b1a-b5c6c7cbeee2",
-        "autoconnect": false,
-        "type": "gsm"
-    },
-    "ppp": {
-        "lcp-echo-failure": 3,
-        "lcp-echo-interval": 30
-    },
-    "gsm": {
-        "number": "*99#",
-        "apn": "plus"
-    }
-};
+//var _defaultConnection = {
+//    "connection": {
+//        "id": "dummyConnection",
+//        "uuid": "8920a029-957d-4da4-9b1a-b5c6c7cbeee2",
+//        "autoconnect": false,
+//        "type": "gsm"
+//    },
+//    "ppp": {
+//        "lcp-echo-failure": 3,
+//        "lcp-echo-interval": 30
+//    },
+//    "gsm": {
+//        "number": "*99#",
+//        "apn": "plus"
+//    }
+//};
+//
+//function _createConnection(data) {
+//    if (!data)
+//        data = _defaultConnection;
+//    return NmManager.addConnection(data);
+//}
 
-function _createConnection(data) {
-    if (!data)
-        data = _defaultConnection;
-    return NmManager.addConnection(data);
-}
-
-describe('devices suite', function(){
+describe('active connection suite', function(){
 
     var deviceId;
 
-    it('get device list', function(done){
-        NmManager.getDevices()
-            .then(function(devices){
-                expect(devices).to.be.an(Array);
-                expect(devices).to.not.be.empty();
-                deviceId = devices[0];
-            })
-            .then(function(){
-                return NmManager.getDeviceProperty(deviceId, "Udi");
-            })
-            .then(function(udi){
-                expect(udi).to.be.ok();
-                return done();
-            })
-    });
+    it('activate gsm connection', function(done){
 
-    it('find gsm list', function(done){
+        var device3g, connection3g;
+
         NmManager.getDevices(8)
             .then(function(devices){
                 expect(devices).to.be.an(Array);
                 expect(devices).to.not.be.empty();
+                device3g = devices[0];
+                return NmManager.getConnections("gsm");
+            })
+            .then(function(connections){
+                expect(connections).to.be.an(Array);
+                expect(connections).to.not.be.empty();
+                connection3g = connections[0];
+                return NmManager.activateConnection(connection3g, device3g);
+            })
+            .then(function(udi){
+                console.log(device3g)
+                console.log(connection3g)
                 return done();
-            });
+            })
     });
 
 })
